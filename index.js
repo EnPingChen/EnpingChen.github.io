fcf329fbb7011548a04cbdac213fafd6278c362b
h34014037 <franksomeboy@gmail.com>
2016-01-26 22:29:03 +0800

    0.0.5

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-	setInterval(function(){
+	var time1 = setInterval(function(){
+		clearInterval(time1);
 		$(".img").fadeIn(1000,function(){
-			$('.content').slideDown();});
+			$('.content').slideDown(1000,function(){
+				$('#reading').slideDown(1000,function(){
+					$("#go").slideDown(1000,function(){
+						$('#dream').slideDown()
+					});
+				})
+			});});
 	},1000)
 })
