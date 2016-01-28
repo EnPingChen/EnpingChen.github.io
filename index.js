8dafab994b302352edba5ab4c2c1275403691d01
h34014037 <franksomeboy@gmail.com>
2016-01-28 19:09:38 +0800

    0.1.0

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
 $(document).ready(function(){
 	var time1 = setTimeout(function(){
+		height();
 		$(".img").fadeIn(1000,function(){
+			height();
 			$('.content').slideDown(1000,function(){
-				$('#reading').slideDown(1000,function(){
-					$("#go").slideDown(1000,function(){
-						$('#dream').slideDown(1000,function(){
-							$('#row1').animate({width:'60%'},1000,function(){
-								$('#row2').animate({width:'80%'},1000,function(){
-									$('#row3').animate({width:'70%'},1000,function(){
-										$('#row4').animate({width:'89%'},1000,function(){
-											$('#row5').animate({width:'60%'})
-										})		
+				height();
+				setTimeout(function(){
+					$('#reading').slideDown(1000,function(){
+						height();
+						$("#go").slideDown(1000,function(){
+							height();
+							$('#dream').slideDown(1000,function(){
+								height();
+								$('#body3').show();
+								$('#row1').animate({width:'60%'},1000,function(){
+									height();
+									$('#row2').animate({width:'80%'},1000,function(){
+										height();
+										$('#row3').animate({width:'70%'},1000,function(){
+											height();
+											$('#row4').animate({width:'89%'},1000,function(){
+												height();
+												$('#row5').animate({width:'60%'},1000,function(){
+													$("#body4").slideDown(1000,function(){
+														height();
+													});
+												})
+											})		
+										})
 									})
 								})
 							})
-						})
-					});
-				})
+						});
+					})
+				},2000)
 			});});
 	},1000)
-	var time2;
-	setTimeout(function(){
-		time2 = setInterval(function(){
-			var height = $(document).height()-$(window).height()
-			$("html, body").animate({scrollTop:height});
-		},2000)
-	},2000)
-	setTimeout(function(){
-		clearInterval(time2);
-		console.log("dhf");
-	},15000)
+	// var time2;
+	// setTimeout(function(){
+	// 	time2 = setInterval(function(){
+	// 		var height = $(document).height()-$(window).height()
+	// 		$("html, body").animate({scrollTop:height});
+	// 	},2000)
+	// },2000)
+	// setTimeout(function(){
+	// 	clearInterval(time2);
+	// 	console.log("dhf");
+	// },10000)
+	// this is for the hover animate
+	$('[data-toggle="tooltip"]').tooltip();   
+
+	function height(){
+		var hei;
+		hei = $(document).height()-$(window).height()
+		$("html, body").animate({scrollTop:hei});
+	}
 })
